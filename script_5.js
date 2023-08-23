8eaf965c20cdc2f091a24e224598b67995f9ecb8
Laura Baizakova <bayzakova_03@mail.ru>
2023-08-23 18:56:51 +0400

    tag

---
+++ script_5.js
@@ -0,0 +1,54 @@
+//  let newEl = document.createElement("span");
+//  newEl.innerHTML = "<h1>Some text</h1>";
+//  let childElement = document.getElementById("childElement");
+//  let child = document.getElementById("childElement");
+//   let childElementClone = child.cloneNode();
+//  let parentDiv = child.parentNode;
+//   parentDiv.insertBefore(newEl, child);
+//  let aBlock = form.appendChild(child);
+//   console.log(aBlock);
+
+// window.innerHeight
+// console.log(window.innerHeight);
+// console.log(window.outerHeight);
+
+// let $btn = document.getElementById('button');
+// let x = window.scrollX;
+// console.log(x);
+// $btn.addEventListener('click', ()=>{
+// alert(x);
+// let screen = window.screenLeft;
+// console.log(screen);
+// })
+
+// window.open('new.html');
+// window.close();
+// window.resizeBy(250,350);
+
+// console.log(navigator.appCodeName);
+// console.log(navigator.language);
+// console.log(navigator.battery);
+// console.log(navigator.getBattery);
+// console.log(navigator.connection);
+// console.log(navigator.cookieEnabled);
+// console.log(navigator.geolocation);
+// console.log(navigator.languages);
+// console.log(navigator.platform);
+// console.log(navigator.product);
+// console.log(navigator.userAgent);
+// console.log(navigator.getUserMedia);
+// console.log(navigator.vibrate);
+
+
+
+console.log(location.hash);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.port);
+
+console.log(location.protocol);
+location.reload();
+location.replace('youtube.com');
+location.assign('new.html');
